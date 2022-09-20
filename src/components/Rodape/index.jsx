1c33beb62d6ff8a5a8d11ef114e333c694f44d44
luiz-ayala123 <luiz.souza7@estudante.ifms.edu.br>
2022-09-20 09:14:52 -0400

    Boca Web

---
+++ src/components/Rodape/index.jsx
@@ -0,0 +1,11 @@
+export const Rodape = () => {
+    return (
+        <div className='rodape'>
+            <footer>
+                <p>Luiz Felipe Ayala Souza</p>
+                <p>Trabalho de Frameworks2</p>
+                <p>João Pedro Assis Ozório</p>
+            </footer>
+        </div>
+    )
+}
